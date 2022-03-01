5c18b1a2c941ef2abf2bd056cf6cc51db063b760
TobeyKwon <tobeykwon@gmail.com>
2022-03-01 22:24:58 +0900

    0301 Chapter3 code

--- core/src/main/java/hello/core/discount/RateDiscountPolicy.java
+++ core/src/main/java/hello/core/discount/RateDiscountPolicy.java
@@ -4,12 +4,12 @@ import hello.core.member.Grade;
 import hello.core.member.Member;
 
 public class RateDiscountPolicy implements DiscountPolicy {
-    private double discountRate = 0.1;
+    private int discountPercent = 10;
 
     @Override
     public int discount(Member member, int price) {
         if(member.getGrade() == Grade.VIP)
-            return (int)(price * discountRate);
+            return price * discountPercent / 100;
         return 0;
     }
 }
--- core/src/test/java/hello/core/member/MemberServiceTest.java
+++ core/src/test/java/hello/core/member/MemberServiceTest.java
@@ -1,10 +1,18 @@
 package hello.core.member;
 
+import hello.core.AppConfig;
 import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
 public class MemberServiceTest {
-    private MemberService memberService = new MemberServiceImpl();
+    MemberService memberService;
+
+    @BeforeEach
+    public void beforeEach() {
+        memberService = new AppConfig().memberService();
+    }
+
     @Test
     void join() {
         //given

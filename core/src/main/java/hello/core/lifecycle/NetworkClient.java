2ea89306e927214691e9b7161bb938e0e1fdd5ed
TobeyKwon <tobeykwon@gmail.com>
2022-03-07 13:03:24 +0900

    0307 Spring2

--- core/src/main/java/hello/core/lifecycle/NetworkClient.java
+++ core/src/main/java/hello/core/lifecycle/NetworkClient.java
@@ -1,9 +1,8 @@
 package hello.core.lifecycle;
 
-import org.springframework.beans.factory.DisposableBean;
-import org.springframework.beans.factory.InitializingBean;
+import org.springframework.context.annotation.Bean;
 
-public class NetworkClient implements InitializingBean, DisposableBean {
+public class NetworkClient  {
 
     private String url;
 
@@ -31,14 +30,12 @@ public class NetworkClient implements InitializingBean, DisposableBean {
         System.out.println("close: " + url);
     }
 
-    @Override
-    public void afterPropertiesSet() throws Exception {
+    public void init() {
         connect();
         call("초기화 메시지");
     }
 
-    @Override
-    public void destroy() throws Exception {
+    public void close() {
         disconnect();
     }
 }
